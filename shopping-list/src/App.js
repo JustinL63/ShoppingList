a638a843900ab33e1b5b98bb19dff6d2fbfd3ce3
Justin Lamb <lambjg63@gmail.com>
2019-02-12 12:00:34 -0500

    file structure

---
+++ shopping-list/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Items from "./items"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          This is where the header will go
+        </header>
+        <p>This is where the body stuff will go</p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ shopping-list/src/items.js
@@ -0,0 +1,43 @@
+
+
+const = Items [
+    {
+        id: 1,
+        name: "Apple",
+        price: "$1",
+        group: "fruit"
+    },
+
+    {
+
+        id: 2,
+        name: "Bacon",
+        price: "$4",
+        group: "Meat"
+    },
+    {
+
+        id: 3,
+        name: "Cheese",
+        price: "$3",
+        group: "Dairy"
+    },
+    {
+
+        id: 4,
+        name: "Coffee",
+        price: "$8",
+        group: "Morning Fuel"
+    },
+    {
+
+        id: 5,
+        name: "Beer",
+        price: "$10",
+        group: "Adult"
+    }
+]
+
+
+
+export default Items
